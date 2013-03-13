7e49a18150c8f6a071d05a4e9e77149705f4fe32
Keyan Hardman <khardman51@gmail.com>
2013-03-13 15:52:37 -0400

    Fixed append

--- app/assets/javascripts/encounters.js
+++ app/assets/javascripts/encounters.js
@@ -2,7 +2,11 @@ $(function() {
   $(".cloud_tag").on('click', function(event) {
     event.preventDefault();
     var txt = $.trim($(this).html());
+
+     if ($(".text-box").html() != '') {
+       $(".text-box").append(", ");
+     }
+
     $(".text-box").append(txt);
-    $(".text-box").append(", ");
   });
 });

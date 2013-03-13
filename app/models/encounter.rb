bdf392251fe7dc9b1e8c7198d2133832bccc17f2
Keyan Hardman <khardman51@gmail.com>
2013-03-13 14:47:55 -0400

    added append to text field on tags

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,7 +1,7 @@
 class Encounter < ActiveRecord::Base
   def self.search(search)
     if search
-      where('name LIKE ?', "%#{search}%")
+      where('name ILIKE ?', "%#{search}%")
     else
       scoped
     end
